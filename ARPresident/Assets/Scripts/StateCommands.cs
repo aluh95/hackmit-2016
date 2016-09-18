61aa8ca6ce0861ef1f5e038bab1f431e85239f3e
Alex Luh <aluh@mit.edu>
2016-09-18 10:33:35 -0400

    Add script for selecting state objects

---
+++ ARPresident/Assets/Scripts/StateCommands.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateCommands : MonoBehaviour {
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect() {
+        Debug.Log(this.name + " was selected");
+    }
+
+}
